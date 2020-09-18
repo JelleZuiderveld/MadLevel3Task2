b422e9a78b766e6bdaa0b20ac0d35c3950c32058
jelle <Jellez@live.com>
2020-09-18 14:17:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/madlevel3task2/AddPortalFragment.kt
@@ -0,0 +1,54 @@
+package com.example.madlevel3task2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_add_portal.*
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+
+const val REQ_PORTAL_KEY = "req_portal"
+const val BUNDLE_PORTAL_KEY = "bundle_portal"
+
+class AddPortalFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_portal, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnAddPortal.setOnClickListener {
+            onAddPortal()
+        }
+    }
+
+    private fun onAddPortal(){
+        val portalText = etPotralName.text.toString()
+
+        if(portalText.isNotBlank()){
+            setFragmentResult(REQ_PORTAL_KEY, bundleOf(Pair(BUNDLE_PORTAL_KEY, portalText)))
+            //setFragmentResult(REQ_PORTAL_KEY, bundleOf(Pair(BUNDLE_PORTAL_KEY, urlText)))
+
+            findNavController().popBackStack()
+        }else{
+            Toast.makeText(activity,R.string.empty, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+}
